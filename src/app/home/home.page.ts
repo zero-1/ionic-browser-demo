1c933947b86b7cec9228ffb4afb0f0d447dc2e66
Arjun Shankar <ashankar@noggin.com.au>
2021-02-02 14:39:00 +1100

    trying to load an external url in an inappbrowser window.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public childbrowser: {};
 
-  constructor() {}
+  constructor(
+    private iab: InAppBrowser,
+    private platform: Platform,
+  ) {
+    this.initializePage();
+  }
+
+  initializePage() {
+    this.platform.ready().then(() => {
+      console.log('PLATFORM IS READY IN WEB PAGE - initializing in constructor');
+      this.openBrowser();
+    });
+  }
+
+  openBrowser(){
+    console.log('OPENING UP 3::');
+    this.childbrowser = this.iab.create('https://app-deep-linking.web.app', '_blank', {
+      location : 'no',//Or 'no'
+      zoom : 'no',//Android only ,shows browser zoom controls
+      hardwareback : 'no',
+      hidden: 'no',
+      clearcache: 'no',
+      beforeload: 'yes',
+      toolbar : 'no', //iOS only22952
+    });
+    
+  //  this.addEventsToChildBrowser();
+  };
 
 }
